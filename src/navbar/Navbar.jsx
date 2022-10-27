51a40d8380f54e5cafb508396f2491e3c14c04fb
bhashkar <bhashkar2600@gmail.com>
2022-10-27 22:13:19 +0530

    thrhrhdededdeds

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import Fitness from "./Fitness";
 import Food from "./Food";
 import Mainnavbar from "./Mainnavbar";
 import Fullcard from "../getfullcard/Fullcard";
-import Data from "../data/Data";
 
 const Bollywood = React.lazy(() => import("./Bollywood"));
 const Navbar = () => {
@@ -32,18 +31,18 @@ const Navbar = () => {
             <Route path="/tourism" element={<Tourismm />} />
             <Route path="/fitness" element={<Fitness />} />
             <Route path="/food" element={<Food />} />
-            <Route path="/tourism/:title" element={<Fullcard Data={Data} />} />
+            <Route path="/tourism/:title" element={<Fullcard  />} />
             <Route
               path="/bollywood/:title"
-              element={<Fullcard Data={Data} />}
+              element={<Fullcard  />}
             />
-            <Route path="/food/:title" element={<Fullcard Data={Data} />} />
-            <Route path="/fitness/:title" element={<Fullcard Data={Data} />} />
+            <Route path="/food/:title" element={<Fullcard  />} />
+            <Route path="/fitness/:title" element={<Fullcard  />} />
             <Route
               path="/technology/:title"
-              element={<Fullcard Data={Data} />}
+              element={<Fullcard  />}
             />
-            <Route path="/home/:title" element={<Fullcard Data={Data} />} />
+            <Route path="/home/:title" element={<Fullcard  />} />
           </Route>
         </Routes>
       </BrowserRouter>
